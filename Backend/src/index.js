ef6107115ee79cc1d9c7ff22091ce93bfb55642c
dhirajacharya <076bct023.dhiraj@pcampus.edu.np>
2024-09-02 12:44:39 +0545

    Initial Setup Of Backend

---
+++ Backend/src/index.js
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+import {app} from './app.js'
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, ()=>{
+    console.log(`Server is starting at:- http://localhost:${port} `)
+})
