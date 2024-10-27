1429f4ebd8589f4adbf2e55141ef6f00cd7ee61c
dheeraaz ach. <076bct023.dhiraj@pcampus.edu.np>
2024-10-27 17:03:01 +0545

    Update constants.js

--- Backend/src/constants/constants.js
+++ Backend/src/constants/constants.js
@@ -3,6 +3,7 @@ export const _DB_Name = "postApp";
 export const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
+  sameSite: "None",
 };
 
 export const maximumImagecount = 6;
